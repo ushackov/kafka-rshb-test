fa040215865bd90da754a61b09e5d704fe8b300f
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-08-08 18:39:27 +0300

    initial - 2

--- producer/src/main/java/ru/vlapin/kafkademo2/KafkaDemo2ProducerApplication.java
+++ producer/src/main/java/ru/vlapin/kafkademo2/KafkaDemo2ProducerApplication.java
@@ -3,18 +3,19 @@ package ru.vlapin.kafkademo2;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j
 @SpringBootApplication
-public class KafkaDemo2Application {
+public class KafkaDemo2ProducerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(KafkaDemo2Application.class, args);
+    SpringApplication.run(KafkaDemo2ProducerApplication.class, args);
   }
 
   @Bean
@@ -25,10 +26,10 @@ public class KafkaDemo2Application {
         .replicas(1).build();
   }
 
-  @KafkaListener(
-      id = "myId",
-      topics = "${kafka-demo.topic-name:topic1}")
-  void listen(String message) {
-    log.info("message: {}", message);
+  @Bean
+  ApplicationRunner runner(@Value("${kafka-demo.topic-name:topic1}") String topicName,
+                           KafkaTemplate<String, String> template) {
+
+    return __ -> template.send(topicName, "Producer starts!");
   }
 }
--- producer/src/test/java/ru/vlapin/kafkademo2/KafkaDemo2ProducerApplicationTests.java
+++ producer/src/test/java/ru/vlapin/kafkademo2/KafkaDemo2ProducerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaDemo2ApplicationTests {
+class KafkaDemo2ProducerApplicationTests {
 
   @Test
   void contextLoads() {
